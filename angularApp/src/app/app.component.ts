a3f46952ffcaa62f4847e3d7046f3e7ff536bc9f
yogendratest <yogendras121@gmail.com>
2018-12-05 16:01:51 +0530

    Angular app added hear :

---
+++ angularApp/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: []
+})
+export class AppComponent {
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo : Todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo : Todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+}
---
+++ angularApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TodoDataService } from './todo-data.service';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoListFooterComponent,
+    TodoListHeaderComponent,
+    TodoListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [TodoDataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
